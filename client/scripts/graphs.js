b750ab0b3ff85cda565c5e8d6e7d50487896d0e8
Andrew White <white.d.andrew@gmail.com>
2016-10-31 21:03:17 -0400

    Finished bridge finding algorithm

--- client/scripts/graphs.js
+++ client/scripts/graphs.js
@@ -5,7 +5,7 @@ exports.set2graph = function(edge_set, map) {
     let g = new glib.Graph({directed: false});
     map.points.forEach( (p, i) => {
 	g.setNode(i, p.name);
-    });
+    });    
     edge_set.forEach((e) => {
 	g.setEdge(e[0], e[1]);
     });
@@ -14,6 +14,36 @@ exports.set2graph = function(edge_set, map) {
 }
 
 exports.bridges = function(g) {
-    
-    return [];
+    //http://stackoverflow.com/questions/11218746/bridges-in-a-connected-graph/11221469#11221469
+
+    var data = {low: Array(), pre: Array(), cnt: 0, bridges: Array()};
+    g.nodes().forEach( () => {
+	data.low.push(-1);
+	data.pre.push(-1);
+    });   
+    bridge_dfs(g, 0, 0, data) //only have to call from first node, since we only consider connected
+    return data.bridges;
 }
+
+function bridge_dfs(g, u, v, data) {
+    data.pre[v] = data.cnt;
+    data.low[v] = data.cnt++;
+    g.nodeEdges(v).forEach(function(e) {
+	let w = Number(e.w);
+	if(w === v)
+	    w = Number(e.v);	
+	if(data.pre[w] === -1) {
+	    bridge_dfs(g, v, w, data);
+	    data.low[v] = Math.min(data.low[v], data.low[w]);
+	    if(data.low[w] === data.pre[w]) {
+		data.bridges.push([v, w]);		
+	    }
+	} else if(w !== u) { //w is an existing one and not u, pop up stack    
+	    data.low[v] = Math.min(data.low[v], data.pre[w]);
+	}
+    });
+    
+}
+
+exports.bridge_dfs = bridge_dfs;
+
--- client/test/testGraphs.js
+++ client/test/testGraphs.js
@@ -1,11 +1,13 @@
 'use strict';
-var expect = require('chai').expect; //assertion library
+var chai = require('chai');
+chai.use(require('chai-things'));
+var expect = chai.expect; //assertion library
 var graphs = require('../scripts/graphs.js');
 var maps = require('../scripts/maps.js');
 
 describe('graphs', function() {
 
-    it('should be able to turn a map into a graph', function(done) {
+    it('should turn a map into a graph', function(done) {
 
 	let m = new maps.Map();
 	m.add([0,0]);
@@ -14,7 +16,7 @@ describe('graphs', function() {
 
     });
 
-    it('should be able to get sum of edge weights', function(done) {
+    it('should get sum of edge weights', function(done) {
 	let m = new maps.Map();
 	m.add([0,0]);
 	m.add([3,4]);
@@ -27,7 +29,7 @@ describe('graphs', function() {
 	done();
     });
 
-    it('should be able to compute the measure of the graph', function(done) {
+    it('should compute the measure of the graph', function(done) {
 	let m = new maps.Map();
 	m.add([0,0]);
 	m.add([3,4]);
@@ -37,14 +39,70 @@ describe('graphs', function() {
 	done();
     });
 
-    it('should be able to return list of cuttable edges', function(done) {
+    it('should return list of cuttable edges', function(done) {
 	let m = new maps.Map();
 	m.add([0,0]);
 	m.add([0,0]);
 	m.add([0,0]);
 	let g = graphs.set2graph([[0,1], [0,2]], m);
-	expect(graphs.bridges(g)).to.include([0,1]);
-
+	let b = graphs.bridges(g);
+	expect(b).to.deep.include.something.that.deep.equals([0,1]);
+	done();
     });
 
+    
+    it('should return empty list when no cuttable edges', function(done) {
+	let m = new maps.Map();
+	m.add([0,0]);
+	m.add([0,0]);
+	m.add([0,0]);
+	let g = graphs.set2graph([[0,1], [1,2], [0,2]], m);
+	let b = graphs.bridges(g);
+	expect(b.length).to.equal(0);
+	done();
+    });
+
+    
+    it('should return list of cuttable edges in complex graph', function(done) {
+
+	/*
+	 *  0 --- 1
+	 *   \   /
+	 *     2
+	 *     |
+	 *     3
+	 *     |
+	 *     4
+	 *   /   \
+	 *  5 --- 6
+	 *
+	 * bridges: [2,3], [3,4]
+	 */
+	
+	let m = new maps.Map();
+	for(let i = 0; i < 7; i++)
+	    m.add([0,0]);
+	
+	let edges = [ [0,1],
+		      [0,2],
+		      [1,2],
+		      [2,3],
+		      [3,4],
+		      [4,5],
+		      [4,6],
+		      [5,6] ];
+	
+	let g = graphs.set2graph(edges, m);
+	let b = graphs.bridges(g);
+	console.log(b);
+	
+	expect(b.length).to.equal(2);
+	expect(b).to.deep.include.something.that.deep.equals([2,3]);
+	expect(b).to.deep.include.something.that.deep.equals([3,4]);
+	done();
+    });
+
+    it('should propose new graph with prob', function(done) {
+	
+    });
 });
